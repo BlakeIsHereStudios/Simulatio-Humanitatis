c8577f130f9f79528ccd530838e3607aefe0a96d
BlakeIsHere <blakeishere458@gmail.com>
2025-12-17 19:38:20 +0000

    Refactor asset paths and improve unit movement
    Moved sprite assets to the Resources directory and updated all references accordingly. Refactored the unit movement script to use velocity-based movement in _physics_process for smoother and more flexible control. Updated input mappings to support gamepad controls and adjusted UI layout for better scaling and positioning.

--- current_unit.gd
+++ current_unit.gd
@@ -1,12 +1,25 @@
 extends CharacterBody2D
 
-func _input(_event):
-	if Input.is_action_just_pressed("up"):
-		position.y -= 16
-	if Input.is_action_just_pressed("down"):
-		position.y += 16
+const speed = 100
 
-	if Input.is_action_just_pressed("left"):
-		position.x -= 32
-	if Input.is_action_just_pressed("right"):
-		position.x += 32
+func _physics_process(delta):
+	player_movement(delta)
+
+func player_movement(delta):
+	if Input.is_action_pressed("up"):
+		velocity.y = -speed
+		velocity.x = 0
+	elif Input.is_action_pressed("down"):
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("right"):
+		velocity.x = speed
+		velocity.y = 0
+	else:
+		velocity.x = 0
+		velocity.y = 0
+
+	move_and_slide ()
